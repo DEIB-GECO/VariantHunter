aa7d2cf832b03f677681a5cbf9309bf34166f59e
Arif Canakoglu <acanak@gmail.com>
2022-04-13 17:47:08 +0200

    added create_docker script

---
+++ create_docker.sh
@@ -0,0 +1,11 @@
+rm -rf docker
+mkdir docker
+cp requirements.txt ./docker/
+cp Dockerfile ./docker/
+
+cp -r ./backend ./docker/
+mkdir ./docker/frontend
+cp -r ./frontend/dist ./docker/frontend/dist
+rm -f docker/backend/varianthunter.db
+cd docker
+DOCKER_BUILDKIT=0 docker build -t gecopolimi/varianthunter  -f Dockerfile .
