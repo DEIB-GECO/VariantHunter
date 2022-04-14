8b9cbc54e60305c8531b29adcbc8a1ef4f7e86ed
Arif Canakoglu <acanak@gmail.com>
2022-04-14 22:12:50 +0200

    updated Nextstrain metadata file positions

--- backend/apis/parsers/NextstrainParser.py
+++ backend/apis/parsers/NextstrainParser.py
@@ -40,7 +40,7 @@ class NextstrainParser(Parser):
             length = int(s[14])
 
             try:
-                n = float(s[29]) / length
+                n = float(s[30]) / length
             except:
                 n = 0.
 
@@ -50,7 +50,7 @@ class NextstrainParser(Parser):
                 sequence_id = self.get_sequence_id()
 
                 self.batch_seqs.append((sequence_id, date, lineage_id, continent_id, country_id, region_id))
-                for aa in s[47].split(","):
+                for aa in s[48].split(","):
                     if aa != '':
                         protein_name, mutation_name = aa.split(":")
                         protein_id = self.get_protein_id(protein_name)
