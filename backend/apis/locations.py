cd613e5ea8cb69c0ebe0464ecf07c351e4d6d018
lucagrammer <luca2.minotti@mail.polimi.it>
2022-11-08 19:44:51 +0100

    Improvements to prepared statements

--- backend/apis/locations.py
+++ backend/apis/locations.py
@@ -85,28 +85,27 @@ def get_locations(o_string):
     con = sqlite3.connect(db_path)
     cur = con.cursor()
     locations = []
-    string = [o_string + '%', '% ' + o_string + '%']
-    strings = [o_string + '%', '% ' + o_string + '%', o_string + '%', '% ' + o_string + '%']
-    stringss = [o_string + '%', '% ' + o_string + '%', o_string + '%', '% ' + o_string + '%', o_string + '%',
-                '% ' + o_string + '%']
+    params = {
+        'first_w': o_string + '%',  # e.g., "Eu" will match "Europe"
+        'middle_w': '% ' + o_string + '%'   # e.g., "Kin" will match "United Kingdom"
+    }
 
     # Fetch continents
     query = f'''    SELECT LO.location
                     FROM locations AS LO JOIN continents AS CO ON LO.location_id=CO.continent_id
-                    WHERE (upper(LO.location) LIKE upper(?)) OR (upper(LO.location) LIKE upper(?));'''
+                    WHERE (upper(LO.location) LIKE upper(:first_w)) OR (upper(LO.location) LIKE upper(:middle_w));'''
     locations.extend([{'value': x[0], 'type': 'continent', 'country': None, 'continent': None} for x in
-                      cur.execute(query, string).fetchall()])
-
+                      cur.execute(query, params).fetchall()])
 
     # Fetch countries
     query = f'''    SELECT LO.location, CON.location
                     FROM locations AS LO
                     JOIN countries AS CO ON LO.location_id=CO.country_id
                     JOIN locations AS CON ON CON.location_id=CO.continent_id
-                    WHERE (upper(LO.location) LIKE upper(?)) OR (upper(LO.location) LIKE upper(?)) 
-                    OR (upper(CON.location) LIKE upper(?)) OR (upper(CON.location) LIKE upper(?));'''
+                    WHERE (upper(LO.location) LIKE upper(:first_w)) OR (upper(LO.location) LIKE upper(:middle_w)) 
+                    OR (upper(CON.location) LIKE upper(:first_w)) OR (upper(CON.location) LIKE upper(:middle_w));'''
     locations.extend([{'value': x[0], 'type': 'country', 'country': None, 'continent': x[1]} for x in
-                      cur.execute(query, strings).fetchall()])
+                      cur.execute(query, params).fetchall()])
 
     # Fetch regions
     query = f'''    SELECT LO.location, COU.location, CON.location
@@ -115,11 +114,11 @@ def get_locations(o_string):
                     JOIN locations AS COU ON COU.location_id=RE.country_id
                     JOIN countries AS CC ON CC.country_id=RE.country_id
                     JOIN locations AS CON ON CON.location_id=CC.continent_id
-                    WHERE (upper(LO.location) LIKE upper(?)) OR (upper(LO.location) LIKE upper(?)) 
-                    OR (upper(COU.location) LIKE upper(?)) OR (upper(COU.location) LIKE upper(?)) 
-                    OR (upper(CON.location) LIKE upper(?)) OR (upper(CON.location) LIKE upper(?));'''
+                    WHERE (upper(LO.location) LIKE upper(:first_w)) OR (upper(LO.location) LIKE upper(:middle_w)) 
+                    OR (upper(COU.location) LIKE upper(:first_w)) OR (upper(COU.location) LIKE upper(:middle_w)) 
+                    OR (upper(CON.location) LIKE upper(:first_w)) OR (upper(CON.location) LIKE upper(:middle_w));'''
     locations.extend([{'value': x[0], 'type': 'region', 'country': x[1], 'continent': x[2]} for x in
-                      cur.execute(query, stringss).fetchall()])
+                      cur.execute(query, params).fetchall()])
 
     con.close()
     return locations
