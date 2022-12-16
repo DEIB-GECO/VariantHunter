00e29d1d3ddd8f6bb960d4a1dbe1045f0742987d
lucagrammer <luca2.minotti@mail.polimi.it>
2022-12-16 10:16:24 +0100

    Added try-out step to the app-tour

--- frontend/src/components/intros/ExplorerIntro.vue
+++ frontend/src/components/intros/ExplorerIntro.vue
@@ -6,8 +6,10 @@
 -->
 
 <template>
-  <feature-intro v-model="visibility" floating step="explorer" :internal-steps="1" next-step="result" :icon="tips[currentTip].icon"
-                 @nextInternalStep="nextInternalStep" :end="currentTip>0" >
+  <feature-intro v-model="visibility" floating step="explorer" :internal-steps="isPublicEndpoint?2:1"
+                 prev-step="definition" next-step="result" :icon="tips[currentTip].icon"
+                 @nextInternalStep="nextInternalStep" :end="currentTip>0"
+                 :next-label="isPublicEndpoint?'I\'ll do it myself':undefined">
     <template>
       <div class="pl-4 mb-6">
         <div>
@@ -18,70 +20,103 @@
                v-html="tips[currentTip].bodyAppend"/>
         </div>
       </div>
+      <div class="d-flex mt-n3 mb-n5" v-if="tips[currentTip].id === 'try'">
+        <v-spacer/>
+        <btn-with-tooltip text="Select example randomly" icon="mdi-chart-box"
+                          :click-handler="selectFeatured" hover-color="secondary"
+                          size="small" content-class="ml-1 mt-1 mt-md-0" top
+                          tip="Run an analysis from the featured ones"/>
+      </div>
     </template>
   </feature-intro>
 </template>
 
 <script>
 import FeatureIntro from "@/components/general/basic/FeatureIntro";
-import {mapMutations} from "vuex";
+import {mapMutations, mapState} from "vuex";
+import BtnWithTooltip from "@/components/general/basic/BtnWithTooltip.vue";
 
 export default {
   name: "ExplorerIntro",
-  components: {FeatureIntro},
+  components: {BtnWithTooltip, FeatureIntro},
   data() {
     return {
       /** Boolean flag set to true if the step is visible */
-      visibility:false,
+      visibility: false,
 
       /** Tips steps */
       tips: [
         {
+          id: 'params',
           icon: 'mdi-star-circle',
           heading: 'Dataset explorer',
           title: 'How to pick the best parameters?',
           body: 'The dataset explorer allows you to view the number of <b>daily collected sequences</b> and the ' +
-                '<b>trend of lineages</b> over time.',
+              '<b>trend of lineages</b> over time.',
           bodyAppend: 'You can move through time and select the analysis period directly from here using the ' +
-                      'button at the bottom. '
+              'button at the bottom. '
         },
         {
+          id: 'note',
           icon: 'mdi-lightbulb-variant-outline',
           heading: '',
           title: 'A side note',
           body: 'Don\'t worry too much about how you choose the analysis parameters; you\'ll be able to ' +
-                'refine them later.\n',
-          bodyAppend: 'Continue the tour by performing an analysis.'
+              'refine them later.\n',
+        },
+        {
+          id: 'try',
+          icon: 'mdi-lifebuoy',
+          heading: '',
+          title: 'Let\'s try to make an analysis!',
+          body: 'Let\'s start with a featured analysis to continue the tour!'
         },
       ],
 
-       /** Current tip step */
+      /** Current tip step */
       currentTip: 0,
     }
+  },
+
+  computed: {
+    ...mapState(['isPublicEndpoint'])
   },
 
   watch: {
     currentTip() {
       this.$vuetify.goTo('#explorer')
     },
-    visibility(newValue){
-      if(newValue){
+    visibility(newValue) {
+      if (newValue) {
         this.$vuetify.goTo('#explorer')
       }
     }
   },
 
   methods: {
-    ...mapMutations(['setLocation','setLocations', 'setDate', 'setTourStep']),
+    ...mapMutations(['setLocation', 'setLocations', 'setDate', 'setTourStep']),
 
     /** Go to the next internal step */
-    nextInternalStep() {
-      this.currentTip++
+    nextInternalStep(diff) {
+      this.currentTip += diff
+
+      if (this.tips[this.currentTip].id === 'try' && this.isPublicEndpoint) {
+        this.$vuetify.goTo('#featured')
+      } else if (this.tips[this.currentTip].id === 'note' && !this.isPublicEndpoint) {
+        this.tips[this.currentTip].bodyAppend = 'Continue the tour by performing an analysis.'
+      }
+    },
+
+    /** Run a featured example and skip the tour intro of the app-tour for the result panel */
+    selectFeatured() {
+      this.setTourStep('filters')
+      const example = document.getElementById('featured-0') // better if lineage-independent
+      if (example) example.click()
     }
   },
 
   mounted() {
-    if(this.visibility){
+    if (this.visibility) {
       this.$vuetify.goTo('#explorer')
     }
   }
