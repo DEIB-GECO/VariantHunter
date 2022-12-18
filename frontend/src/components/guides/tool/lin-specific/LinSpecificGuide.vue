77bea6c09fed0359b8b11100bb6b337b2b4351c5
lucagrammer <luca2.minotti@mail.polimi.it>
2022-12-18 10:25:49 +0100

    Create LinSpecificGuide.vue

---
+++ frontend/src/components/guides/tool/lin-specific/LinSpecificGuide.vue
@@ -0,0 +1,134 @@
+<!--
+
+  Component:    LinSpecificGuide
+  Description:  Usage guide for the lineage dependent search feature
+
+-->
+
+<template>
+  <function-guide title='Discover <b>lineage-specific</b> search'>
+
+    <!-- Function guide -->
+    <template v-slot:default>
+      <paragraph :src='formImg' left larger-img>
+        <p>
+          In this analysis the user is required to specify one or more  lineages of interest,
+          in addition to the other search parameters.<br/>
+          Multiple lineages can be specified simply by entering their name in the lineage-selection form.
+        </p>
+        <p>
+          <i>In this example we choose the lineage BA.2.</i>
+        </p>
+      </paragraph>
+
+      <paragraph :src='tableImg' right larger-img>
+        <p>
+          The same visual elements are shown as in the previous analysis mode.<br/>
+          Here, however, counts shown for each amino-acid change  are reported only for the selected lineage(s).
+        </p>
+        <p>
+          As shown in the figure, in this mode <b>amino acid changes</b> that are <b>characteristic of the selected
+          lineage</b> (common to at least 50% of the sequences of the database assigned to
+          that lineage) are <span class='highlight'>highlighted in yellow</span>.
+        </p>
+      </paragraph>
+
+      Here, the <b>P-values</b> in the table are computed using a Chi-square test of independence of
+      variables in a contingency table. They represent:
+      <ul>
+        <li>
+          <i>"P-value with mut"</i>: if the population «with mutation» is growing differently compared
+          to the entire population of genome sequences;
+        </li>
+        <li>
+          <i>"P-value without mut"</i>: if the population «without mutation» is growing differently compared
+          to the entire population of genome sequences;
+        </li>
+        <li>
+          <i>"P-value comparative"</i>: if the population «with mutation» is growing differently compared
+          to the population «without mutation».
+        </li>
+      </ul>
+
+      <paragraph :src='heatmapImg' left larger-img>
+        <p>
+          By selecting in the table only the amino-acid changes that are
+          <span class='highlight'>highlighted in yellow</span>
+          <i>(i.e., characterizing for BA.2)</i> we observe the following <b>Heatmap</b>,
+          where most mutations are fixed for four weeks, while 8 of them (at the top
+          of the heatmap) have increased their prevalence from  about 75% to almost 100%.
+        </p>
+      </paragraph>
+
+      <paragraph :src='diffusionImg' right>
+        <p>The trend is clearly shown in the <b>Diffusion Trend</b> chart.</p>
+      </paragraph>
+
+      <paragraph :src='oddImg' right>
+        <p>
+          By looking at the <b>Log<sub>2</sub> Odd Ratio</b> plots we can appreciate an interesting trend of these 8 mutations
+          (hovering on the dots, the details on the diffusion's growth are shown).
+        </p>
+      </paragraph>
+
+       <paragraph :src='cmdImg' left>
+        <p><del>By using the <span class='command'>Next/Prev Week</span> buttons below the plot, a user
+          can <b>navigate in time</b> replicating the same analysis using a four-week period shifted of
+          one week ahead or behind.</del>
+          TESTO MANCANTE/ DA RIFORMULARE PER LE NUOVE OPZIONI <!-- TODO -->
+        </p>
+      </paragraph>
+
+      <paragraph :src='odd2Img' right>
+        <p>
+          <i>
+            In the figure, we show the selected mutations when the <span class='command'>Next Week</span>
+            is analyzed, including also the week of March 27th, 2022.
+          </i>
+        </p>
+      </paragraph>
+      <p>
+        This specific mode of analysis can support the discovery of new sub-lineages. We provide  relevant
+        use cases captured at different times of the use of Variant Hunter: three new sub-lineages were
+        identified during April 2022, whereas two new sub-lineages were identified in December 2022.
+      </p>
+    </template>
+
+    <!-- Use cases -->
+    <template v-slot:use-cases>
+
+      <lin-spec-use-case3/>
+
+    </template>
+
+  </function-guide>
+</template>
+
+<script>
+import FunctionGuide from '@/components/guides/tool/FunctionGuide.vue'
+import Paragraph from '@/components/general/basic/Paragraph.vue'
+import LinSpecUseCase3 from "@/components/guides/tool/lin-specific/uc/LinSpecUseCase3.vue";
+
+export default {
+  name: 'LinSpecificGuide',
+  components: {LinSpecUseCase3, Paragraph, FunctionGuide},
+
+  data() {
+    return {
+      /** Images */
+      formImg: require('@/assets/guide/lin_dep/form.png'),
+      tableImg: require('@/assets/guide/lin_dep/table.png'),
+      heatmapImg: require('@/assets/guide/lin_dep/heatmap.png'),
+      diffusionImg: require('@/assets/guide/lin_dep/diffusion.png'),
+      oddImg: require('@/assets/guide/lin_dep/odd.png'),
+      cmdImg: require('@/assets/guide/lin_dep/cmd.png'),
+      odd2Img: require('@/assets/guide/lin_dep/odd2.png'),
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
