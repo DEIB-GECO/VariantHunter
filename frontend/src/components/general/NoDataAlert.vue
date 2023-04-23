0a43d36072067607f489012f7e8dc6e863280ef3
lucagrammer <luca2.minotti@mail.polimi.it>
2023-04-23 11:36:14 +0200

    Update no data alert message

--- frontend/src/components/general/NoDataAlert.vue
+++ frontend/src/components/general/NoDataAlert.vue
@@ -1,7 +1,7 @@
 <!--
 
   Component:    NoDataAlert
-  Description:  App preferences panel
+  Description:  No data alert message
 
   Props:
   └── value: Value variable for binding of the value
@@ -17,10 +17,13 @@
       <div>
         <div class="text-h5 font-weight-black">Sorry,&nbsp;<span
             class="font-weight-bold">your search produced no results.</span></div>
-        <div class="text-body-1 mt-1 mb-6">This is due to the lack of data in the selected period of analysis.</div>
 
-        <div class="text-uppercase text-body-4 font-weight-black">What to do?</div>
-        <div>Try selecting a larger geographic area, or a time period that includes more sequences.
+        <div class="text-body-1 mt-1 mb-5 hidden-xs-only">For your analysis to be run, the last 7 days of the selected
+          period should present <b>at least one sequence</b>.
+        </div>
+
+        <div class="text-uppercase text-body-4 font-weight-black hidden-xs-only">What to do?</div>
+        <div class="text-body-3 hidden-xs-only">Try selecting a larger geographic area, or a time frame that includes more sequences.
           Using the Dataset Explorer can help you in this regard.
         </div>
       </div>
